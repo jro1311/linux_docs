3b2bad6aff47eb0711a51fffdf36318cfe0c5beb
jro1311 <robakjoshua@gmail.com>
2025-05-18 20:20:24 -0500

    Add files via upload

--- scripts/smart_info_export.sh
+++ scripts/smart_info_export.sh
@@ -4,9 +4,9 @@
 set -euo pipefail
 
 # Exports smart info
-sudo smartctl -a /dev/nvme0  > $HOME/Documents/linux_docs/documentation/smart_info/$(date +%Y-%m).md
-sudo smartctl -a /dev/sda  >> $HOME/Documents/linux_docs/documentation/smart_info/$(date +%Y-%m).md
-sudo smartctl -a /dev/sdb  >> $HOME/Documents/linux_docs/documentation/smart_info/$(date +%Y-%m).md
+sudo smartctl -a /dev/nvme0  > $HOME/Documents/linux_docs/documentation/smart_info/$(date +%Y-%m).txt
+sudo smartctl -a /dev/sda  >> $HOME/Documents/linux_docs/documentation/smart_info/$(date +%Y-%m).txt
+sudo smartctl -a /dev/sdb  >> $HOME/Documents/linux_docs/documentation/smart_info/$(date +%Y-%m).txt
 
 # Prints a conclusive message to end the script
 echo "SMART info has been successfully exported."
