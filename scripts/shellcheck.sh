dd5ade57eefae53b0a0556df5a29aa1735fcff32
jro1311 <robakjoshua@gmail.com>
2025-06-07 21:48:31 -0500

    Add files via upload

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -3,6 +3,31 @@
 # Sets the script to exit immediately when any error, unset variable, or pipeline failure occurs
 set -euo pipefail
 
+# Conditional execution based on if the package is installed
+if ! command -v shellcheck &> /dev/null; then
+    # Installs package(s) based on the package manager detected
+    if command -v pacman &> /dev/null; then
+        echo "Detected: pacman"
+        # Installs package(s)
+        sudo pacman -Syu --needed --noconfirm shellcheck
+    elif command -v apt &> /dev/null; then
+        echo "Detected: apt"
+        # Installs package(s)
+        sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y shellcheck
+    elif command -v dnf &> /dev/null; then
+        echo "Detected: dnf"
+        # Installs package(s)
+        sudo dnf upgrade -y && sudo dnf install -y shellcheck
+    elif command -v zypper &> /dev/null; then
+        echo "Detected: zypper"
+        # Installs package(s)
+        sudo zypper ref && sudo zypper -y dup && sudo zypper in -y shellcheck
+    else
+        echo "Unknown package manager"
+        exit 1
+    fi
+fi
+
 # Track if any script fails the syntax check
 error_found=0
 
