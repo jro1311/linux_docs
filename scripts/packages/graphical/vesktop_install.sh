a084b5bf8c51064e216e0fdf129c80c16178b258
jro1311 <robakjoshua@gmail.com>
2025-06-23 22:06:03 -0500

    Add files via upload

---
+++ scripts/packages/graphical/vesktop_install.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# Sets the script to exit immediately when any error, unset variable, or pipeline failure occurs
+set -euo pipefail
+
+# Checks for flatpak and flathub
+if ! command -v flatpak &> /dev/null || ! flatpak remote-list | grep -q "flathub"; then
+    # Runs script to install flatpak and set up flathub
+    chmod +x "$HOME/Documents/linux_docs/scripts/packages/terminal/flatpak_install.sh"
+    "$HOME/Documents/linux_docs/scripts/packages/terminal/flatpak_install.sh"
+fi
+
+# Installs package(s) based on the package manager detected
+if command -v pacman &> /dev/null; then
+    echo "Detected: pacman"
+    # Checks for Chaotic AUR
+    if ! grep -q 'chaotic' /etc/pacman.conf; then
+        sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+        sudo pacman-key --lsign-key 3056513887B78AEB
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+        sudo tee -a /etc/pacman.conf <<-'EOF'
+        [chaotic-aur]
+            Include = /etc/pacman.d/chaotic-mirrorlist
+
+EOF
+    fi
+    
+    # Checks for paru
+    if command -v paru > /dev/null 2>&1; then
+        # Installs package(s)
+        paru -Syu vesktop
+    fi
+
+    # Checks for yay
+    if command -v yay > /dev/null 2>&1; then
+        # Installs package(s)
+        yay -Syu vesktop
+    else
+        sudo pacman -Syu --needed --noconfirm base-devel git makepkg
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si --noconfirm
+        cd ..
+        rm -rf yay
+        
+        # Installs package(s)
+        yay -Syu vesktop
+    fi
+elif command -v apt &> /dev/null; then
+    echo "Detected: apt"
+    # Installs package(s)
+    flatpak update -y && flatpak install flathub -y vesktop
+elif command -v dnf &> /dev/null; then
+    echo "Detected: dnf"
+    # Installs package(s)
+    flatpak update -y && flatpak install flathub -y vesktop
+elif command -v zypper &> /dev/null; then
+    echo "Detected: zypper"
+    # Installs package(s)
+    flatpak update -y && flatpak install flathub -y vesktop
+elif command -v xbps-install &> /dev/null; then
+    echo "Detected: xbps"
+    # Installs package(s)
+    flatpak update -y && flatpak install flathub -y vesktop
+else
+    echo "Unsupported package manager"
+    # Installs package(s)
+    flatpak update -y && flatpak install flathub -y vesktop
+fi
+
+# Prints a conclusive message
+echo "Vesktop is now installed"
+read -p "Press enter to exit"
