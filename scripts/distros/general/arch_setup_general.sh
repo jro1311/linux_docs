f25a4da021dcef4bce831424ca3f3e359ba1fafe
jro1311 <robakjoshua@gmail.com>
2025-06-28 09:17:03 -0500

    Add files via upload

--- scripts/distros/general/arch_setup_general.sh
+++ scripts/distros/general/arch_setup_general.sh
@@ -241,8 +241,8 @@ case "$desktop" in
         ;;
 esac
 
-# Removes all cached versions of packages except the latest version
-sudo paccache -rk0
+# Removes all cached versions of packages except the latest and one prior version
+sudo paccache -rk1
 
 # Checks for init system
 if ps -p 1 -o comm= | grep -q "systemd"; then
