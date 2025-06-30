caf08aa8ee265f75689d5468aa66c7a929f5b807
jro1311 <robakjoshua@gmail.com>
2025-06-29 20:59:00 -0500

    Add files via upload

--- scripts/enable_xorg_vrr.sh
+++ scripts/enable_xorg_vrr.sh
@@ -3,21 +3,14 @@
 # Sets the script to exit immediately when any error, unset variable, or pipeline failure occurs
 set -euo pipefail
 
-# Checks for X11
+# Get GPU information
+gpu_info=$(lspci | grep -E "VGA|3D")
+
+# Checks for session type
 if [ "$XDG_SESSION_TYPE" = "x11" ]; then
     echo "Detected Session: X11"
-    # Get GPU information
-    gpu_info=$(lspci | grep -E "VGA|3D")
-    
-    # Checks for Intel GPU
-    if echo "$gpu_info" | grep -i "intel" &> /dev/null; then
-        echo "Detected GPU: Intel"
-        echo "VRR should be automatically enabled as long as your monitor supports it"
-        read -p "Press enter to exit"
-        exit 0
-        
     # Checks for AMD GPU
-    elif echo "$gpu_info" | grep -i "amd" &> /dev/null; then
+    if echo "$gpu_info" | grep -i "amd" &> /dev/null; then
         echo "Detected GPU: AMD"
         # Creates manual config
         sudo tee /etc/X11/xorg.conf.d/20-amdgpu.conf <<- 'EOF'
@@ -33,20 +26,12 @@ if [ "$XDG_SESSION_TYPE" = "x11" ]; then
         EndSection
         
 EOF
-
-    # Checks for Nvidia GPU
-    elif echo "$gpu_info" | grep -i "nvidia" &> /dev/null; then
-        echo "Detected GPU: Nvidia"
-        echo "VRR should be automatically enabled as long as your monitor supports it"
+    else
+        echo "No AMD GPU detected"
+        echo "Nothing to do"
         read -p "Press enter to exit"
         exit 0
-    else
-        echo "Unknown GPU"
-        read -p "Press enter to exit"
-        exit 1
     fi
-    
-# Checks for Wayland
 elif [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     echo "Detected Session: Wayland"
     echo "Nothing to do"
