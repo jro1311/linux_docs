29caa19dfb1f392f524fea5919924bd04eabceb5
jro1311 <robakjoshua@gmail.com>
2025-10-23 15:49:04 -0500

    Add files via upload

--- scripts/shellcheck_scripts.sh
+++ scripts/shellcheck_scripts.sh
@@ -76,8 +76,8 @@ error_found=0
 
 # Recursively finds all .sh files and checks each for errors
 while IFS= read -r -d '' script; do
-    if ! shellcheck -x --exclude=2162 "$script" > /dev/null 2>&1; then
-        shellcheck -x --exclude=2162 "$script"
+    if ! shellcheck -x "$script" > /dev/null 2>&1; then
+        shellcheck -x "$script"
         error_found=1
     fi
 done < <(find "$HOME/Documents/linux_docs/scripts" -type f -name '*.sh' -print0)
