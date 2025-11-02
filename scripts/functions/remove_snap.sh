2913b531aa7ba77c0f5b565a8b7746723ea220ce
jro1311 <robakjoshua@gmail.com>
2025-11-02 11:29:47 -0600

    Add files via upload

--- scripts/functions/remove_snap.sh
+++ scripts/functions/remove_snap.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 # Define text colors
 red=$(tput setaf 1)
 green=$(tput setaf 2)
+yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
 # Checks for init system
@@ -80,78 +81,86 @@ fi
 packages=("snapd")
 aur_packages=("snapd")
 
-# Disables snap daemon
-sudo systemctl disable --now snapd
+# Checks for package
+if command -v snap > /dev/null 2>&1; then
 
-# Removes user-installed package(s)
-for user_package in $(snap list | awk '!/^Name/ {print $1}' | grep -v -E '^(bare|core|core18|core20|core22|snapd|gtk-common-themes)$'); do
-    sudo snap remove --purge "$user_package"
-done
+    # Disables snap daemon
+    sudo systemctl disable --now snapd
 
-# Removes theme/base package(s)
-for base_package in gtk-common-themes bare core core18 core20 core22 snapd; do
-    if snap list | grep -q "^$base_package "; then
-        sudo snap remove --purge "$base_package"
-    fi
-done
+    # Removes user-installed package(s)
+    for user_package in $(snap list | awk '!/^Name/ {print $1}' | grep -v -E '^(bare|core|core18|core20|core22|snapd|gtk-common-themes)$'); do
+        sudo snap remove --purge "$user_package"
+    done
 
-# Checks for package manager and removes package(s)
-if [ "$primary_package_manager" = "apt" ]; then
+    # Removes theme/base package(s)
+    for base_package in gtk-common-themes bare core core18 core20 core22 snapd; do
+        if snap list | grep -q "^$base_package "; then
+            sudo snap remove --purge "$base_package"
+        fi
+    done
 
-    sudo apt-get purge -y "${packages[@]}"
+    # Checks for package manager and removes package(s)
+    if [ "$primary_package_manager" = "apt" ]; then
 
-    # Locks package(s) from being reinstalled automatically
-    if ! apt-mark showhold | grep -q "^snapd$"; then
-        sudo apt-mark hold snapd
-    fi
+        sudo apt-get purge -y "${packages[@]}"
 
-elif [ "$primary_package_manager" = "dnf" ]; then
-    sudo dnf remove -y "${packages[@]}"
+        # Locks package(s) from being reinstalled automatically
+        if ! apt-mark showhold | grep -q "^snapd$"; then
+            sudo apt-mark hold snapd
+        fi
 
-elif [ "$primary_package_manager" = "pacman" ]; then
+    elif [ "$primary_package_manager" = "dnf" ]; then
+        sudo dnf remove -y "${packages[@]}"
 
-    # Checks for AUR package manager
-    if [ "$aur_package_manager" != "unknown" ]; then
-        "$aur_package_manager" -Rs --noconfirm "${aur_packages[@]}"
-    else
-        sudo pacman -S --needed --noconfirm base-devel git makepkg
-        git clone https://aur.archlinux.org/paru.git
-        cd paru
-        makepkg -si --noconfirm
-        cd ..
-        rm -rf paru
-        paru -Rs --noconfirm "${aur_packages[@]}"
-    fi
+    elif [ "$primary_package_manager" = "pacman" ]; then
 
-elif [ "$primary_package_manager" = "zypper" ]; then
+        # Checks for AUR package manager
+        if [ "$aur_package_manager" != "unknown" ]; then
+            "$aur_package_manager" -Rs --noconfirm "${aur_packages[@]}"
+        else
+            sudo pacman -S --needed --noconfirm base-devel git makepkg
+            git clone https://aur.archlinux.org/paru.git
+            cd paru
+            makepkg -si --noconfirm
+            cd ..
+            rm -rf paru
+            paru -Rs --noconfirm "${aur_packages[@]}"
+        fi
+
+    elif [ "$primary_package_manager" = "zypper" ]; then
         sudo zypper rm --clean-deps -y "${packages[@]}"
 
         # Removes repo(s)
         sudo zypper rr snappy
 
-elif [ "$primary_package_manager" = "rpm-ostree" ]; then
+    elif [ "$primary_package_manager" = "rpm-ostree" ]; then
 
-    if command -v "${packages[@]}" > /dev/null 2>&1; then
-        sudo rpm-ostree remove "${packages[@]}"
+        if command -v "${packages[@]}" > /dev/null 2>&1; then
+            sudo rpm-ostree remove "${packages[@]}"
+        fi
+
+    else
+        echo "${red}Unsupported package manager ${reset}"
+        exit 1
     fi
 
+    # Removes directory(s)
+    if [ -d /var/cache/snapd ]; then
+        sudo rm -rfv /var/cache/snapd
+    fi
+
+    if [ -d /snap ]; then
+        sudo rm -rfv /snap
+    fi
+
+    if [ -d "$HOME/snap" ]; then
+        rm -rfv "$HOME/snap"
+    fi
+
+    # Prints a conclusive message
+    echo "${green}Snap removed ${reset}"
+
 else
-    echo "${red}Unsupported package manager ${reset}"
+    echo "${yellow}Snap not detected ${reset}"
     exit 1
 fi
-
-# Removes directory(s)
-if [ -d /var/cache/snapd ]; then
-    sudo rm -rfv /var/cache/snapd
-fi
-
-if [ -d /snap ]; then
-    sudo rm -rfv /snap
-fi
-
-if [ -d "$HOME/snap" ]; then
-    rm -rfv "$HOME/snap"
-fi
-
-# Prints a conclusive message
-echo "${green}Snap removed ${reset}"
