da666c492ffa8646d4e0a8bf6f1865ace8849d2b
jro1311 <robakjoshua@gmail.com>
2026-02-17 17:18:06 -0600

    Add files via upload

--- configs/system/bash/bashrc.d/list_locked.sh
+++ configs/system/bash/bashrc.d/list_locked.sh
@@ -17,71 +17,69 @@ list_locked_toolbox_pkg() { toolbox run sudo dnf versionlock list; }
 list_locked() {
     local managers=(apt dnf eopkg pacman xbps zypper flatpak snap toolbox rpm-ostree)
 
-    for package in "$@"; do
-        for manager in "${managers[@]}"; do
-            local listing_locked="${green}Listing locked packages using $manager... ${reset}"
-            local no_function_available="${yellow}Function not available using $manager. ${reset}"
+    for manager in "${managers[@]}"; do
+        local listing_locked="${green}Listing locked packages using $manager... ${reset}"
+        local no_function_available="${yellow}Function not available using $manager. ${reset}"
 
-            case "$manager" in
-                "apt")
-                    if [ "$primary_package_manager" = "apt" ]; then
-                        echo "$listing_locked"
-                        list_locked_apt
-                    fi
-                    ;;
-                "dnf")
-                    if [ "$primary_package_manager" = "dnf" ]; then
-                        echo "$listing_locked"
-                        list_locked_dnf
-                    fi
-                    ;;
-                "eopkg")
-                    if [ "$primary_package_manager" = "eopkg" ]; then
-                        no_function_available
-                    fi
-                    ;;
-                "pacman")
-                    if [ "$primary_package_manager" = "pacman" ]; then
-                        echo "$listing_locked"
-                        list_locked_pacman
-                    fi
-                    ;;
-                "xbps")
-                    if [ "$primary_package_manager" = "xbps" ]; then
-                        echo "$listing_locked"
-                        list_locked_xbps
-                    fi
-                    ;;
-                "zypper")
-                    if [ "$primary_package_manager" = "zypper" ]; then
-                        echo "$listing_locked"
-                        list_locked_zypper
-                    fi
-                    ;;
-                "flatpak")
-                    if [ "$flatpak_installed" -eq 1 ]; then
-                        echo "$listing_locked"
-                        list_locked_flatpak_pkg
-                    fi
-                    ;;
-                "snap")
-                    if [ "$snap_installed" -eq 1 ]; then
-                        echo "$listing_locked"
-                        list_locked_snap_pkg
-                    fi
-                    ;;
-                "toolbox")
-                    if [ "$toolbox_installed" -eq 1 ]; then
-                        echo "$listing_locked"
-                        list_locked_toolbox_pkg
-                    fi
-                    ;;
-                "rpm-ostree")
-                    if [ "$primary_package_manager" = "rpm-ostree" ]; then
-                        no_function_available
-                    fi
-                    ;;
-            esac
-        done
+        case "$manager" in
+            "apt")
+                if [ "$primary_package_manager" = "apt" ]; then
+                    echo "$listing_locked"
+                    list_locked_apt
+                fi
+                ;;
+            "dnf")
+                if [ "$primary_package_manager" = "dnf" ]; then
+                    echo "$listing_locked"
+                    list_locked_dnf
+                fi
+                ;;
+            "eopkg")
+                if [ "$primary_package_manager" = "eopkg" ]; then
+                    no_function_available
+                fi
+                ;;
+            "pacman")
+                if [ "$primary_package_manager" = "pacman" ]; then
+                    echo "$listing_locked"
+                    list_locked_pacman
+                fi
+                ;;
+            "xbps")
+                if [ "$primary_package_manager" = "xbps" ]; then
+                    echo "$listing_locked"
+                    list_locked_xbps
+                fi
+                ;;
+            "zypper")
+                if [ "$primary_package_manager" = "zypper" ]; then
+                    echo "$listing_locked"
+                    list_locked_zypper
+                fi
+                ;;
+            "flatpak")
+                if [ "$flatpak_installed" -eq 1 ]; then
+                    echo "$listing_locked"
+                    list_locked_flatpak_pkg
+                fi
+                ;;
+            "snap")
+                if [ "$snap_installed" -eq 1 ]; then
+                    echo "$listing_locked"
+                    list_locked_snap_pkg
+                fi
+                ;;
+            "toolbox")
+                if [ "$toolbox_installed" -eq 1 ]; then
+                    echo "$listing_locked"
+                    list_locked_toolbox_pkg
+                fi
+                ;;
+            "rpm-ostree")
+                if [ "$primary_package_manager" = "rpm-ostree" ]; then
+                    no_function_available
+                fi
+                ;;
+        esac
     done
 }
