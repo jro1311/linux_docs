979218f8672aed13066f5b7b1b2615153c9e5b0a
jro1311 <robakjoshua@gmail.com>
2026-03-31 18:52:03 -0500

    Add files via upload

--- help/bash/display.sh
+++ help/bash/display.sh
@@ -21,10 +21,11 @@ display=$("$display_cmd" | grep "primary" -A1 | tail -1 | awk '{print $1}')
 display_w=$(echo "$display" | cut -d'x' -f1)
 display_h=$(echo "$display" | cut -d'x' -f2)
 refresh_rate=$("$display_cmd"  | grep "primary" -A1 | tail -1 | awk '{print $2}' | sed 's/[*+]//g' | xargs printf "%.0f")
-max_fps_target=$(awk "BEGIN {printf \"%.0f\", int(($refresh_rate - 5) / 10 + 0.5) * 10}")
+max_fps_target="$(awk "BEGIN {printf \"%.0f\", int(($refresh_rate - 5) / 5 + 0.5) * 5}")"
 
 # Prints display information
 echo "${green}Display Resolution:${reset} $display"
 echo "${green}Display Width:${reset} $display_w"
 echo "${green}Display Height:${reset} $display_h"
 echo "${green}Display Refresh Rate:${reset} $refresh_rate Hz"
+echo "${green}Max FPS Target:${reset} $max_fps_target FPS"
