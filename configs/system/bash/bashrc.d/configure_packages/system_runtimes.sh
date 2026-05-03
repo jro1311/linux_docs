840c73e83f49627b4c87e767037d05e6f6877884
jro1311 <robakjoshua@gmail.com>
2026-05-02 19:09:59 -0500

    Fixed manual zram configuration.

--- configs/system/bash/bashrc.d/configure_packages/system_runtimes.sh
+++ configs/system/bash/bashrc.d/configure_packages/system_runtimes.sh
@@ -78,7 +78,9 @@ _configure_zram_manual() {
     local algo="$1"
     local memory_bytes="$2"
 
-    sudo rm -f /etc/modules-load.d/zram.conf
+    if [ ! -f /etc/modules-load.d/zram.conf ]; then
+        echo zram | sudo tee /etc/modules-load.d/zram.conf
+    fi
 
     sudo mkdir -p /etc/udev/rules.d
     echo 'ACTION=="add", KERNEL=="zram0", ATTR{initstate}=="0", ATTR{comp_algorithm}="'"$algo"'", ATTR{disksize}="'"$memory_bytes"'"' \
